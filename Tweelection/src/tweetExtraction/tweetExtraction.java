2a07898a0dce4d0996eae34f3b6d56d84e00f136
Arthur Talpaert <arthur.talpaert@isen-lille.fr>
2017-04-11 14:09:02 +0000

    tweetExtraction.java edited online with Bitbucket

--- Tweelection/src/tweetExtraction/tweetExtraction.java
+++ Tweelection/src/tweetExtraction/tweetExtraction.java
@@ -5,10 +5,6 @@
  */
 package tweetExtraction;
 
-import java.util.List;
-import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
-
 /**
  *
  * @author arthur
@@ -16,34 +12,8 @@ import twitter4j.conf.ConfigurationBuilder;
 public class tweetExtraction {
 
     public static void main(String[] args) {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-        .setOAuthConsumerKey("ZroXRDzuSrvwL6hYhgizrkQtR")
-        .setOAuthConsumerSecret("OpK1ze2cdxM9ImgFdPsuhJaxwJJSld2rPrkjK7AZdDisFeCn1C")
-        .setOAuthAccessToken("849890633770815488-wstUiGK2ydWmCcJdpdhjo8mnUBoobrw")
-        .setOAuthAccessTokenSecret("TLho2WgfGxtmWNB3o9BLVxqb1rJzJmBLHmbyDFgCNit63");
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
-        //Twitter twitter = TwitterFactory.getSingleton();
-        List<Status> statuses;
-        try {
-            /*statuses = twitter.getHomeTimeline();
-            System.out.println("Showing home timeline.");
-            for (Status status : statuses) {
-                System.out.println(status.getUser().getName() + ":"
-                        + status.getText());
-            }*/
-            Query query = new Query("fillon");
-            QueryResult result = twitter.search(query);
-            int b= 0;
-            for (Status status : result.getTweets()) {
-                b+=1;
-                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-            }
-            System.out.println(b);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        TweetAboutCandidate tweetAboutFillon = new TweetAboutCandidate("fillon");
+        tweetAboutFillon.extractTweets(100);
     }
 
 }
