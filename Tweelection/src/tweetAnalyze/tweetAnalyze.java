4ea126fbf3eb0c0b007c3c50c0a35e25d8e3acb4
Gérald Gallet <gerald.gallet@isen-lille.fr>
2017-05-02 17:16:42 +0200

    CREATE: function to analyze tweets in real time

--- Tweelection/src/tweetAnalyze/tweetAnalyze.java
+++ Tweelection/src/tweetAnalyze/tweetAnalyze.java
@@ -28,6 +28,10 @@ public class tweetAnalyze {
     private ArrayList<double[]> rates;
     private ArrayList<double[]> numberAnalyzed;
     private List<String> period;
+    
+    private int threshold = 10;
+    
+    private Graph g;
     /*
         rates.get(0) contient les notes de namesOfSubject.get(0) pour la periode entière
         Rates.get(0)[0] contient la note de names.get(0) pour period.get(0),
@@ -42,6 +46,7 @@ public class tweetAnalyze {
         rates = new ArrayList<>();
         numberAnalyzed = new ArrayList<>();
         //period = new List<>();
+        Graph g = new Graph();
         
         bag = new BagOfWords();
     }
@@ -50,6 +55,7 @@ public class tweetAnalyze {
     public void setRealTime(boolean realTime) { this.realTime = realTime; }
     public void setPeriod(List<String> period) { this.period = period; }
     public void setBogFileName(String bogFileName) { this.bogFileName = bogFileName; }
+    public void setThreshold(int threshold) { this.threshold = threshold; }
     
     /* My set of getters */
     public boolean getRealTime() { return realTime; }
@@ -58,29 +64,71 @@ public class tweetAnalyze {
     public String getPeriodByIndex(int index) { return period.get(index); }
     public String getBogFileName() { return bogFileName; }
     
+    public int getIndexByName(String name) {
+        for(int i = 0; i < namesOfSubjects.size(); i++) {
+            if(name.equals(namesOfSubjects.get(i)))
+                return i;
+        }
+        
+        return -1;
+    }
+    
     /* Adds a new subject to search */
     public void addSubject(String subject) {
         namesOfSubjects.add(subject);
     }
     
     /* Sets the good parameters */
-    public void setUp() {
+    public void setUp(int size) {
         for(int i = 0; i < getNumberOfSubjects(); i++) {
-            rates.add(new double[period.size()]);
-            numberAnalyzed.add(new double[period.size()]);
+            rates.add(new double[size]);
+            numberAnalyzed.add(new double[size]);
         }
     }
     
     /* Fonction called to launch the analyze of the subjects */
     public void launchAnalyze() {
         if(realTime)
-            launchAnalyzeRealTime();
+            System.out.println("Real time");
+            //launchAnalyzeRealTime();
         else
             launchAnalyzeFiles();
     }
     
-    public void launchAnalyzeRealTime() {
-        
+    public void launchAnalyzeRealTime(ArrayList<String> tweets) {
+        try {
+            bag.setFileName(bogFileName);
+            bag = new BagOfWords(bag.deserialize());
+            
+            int subjectIndex = getIndexByName(tweets.get(0));
+            if(rates.get(0).length == 0 || rates.get(0) == null)
+                setUp(1);
+
+            int numberOfTweets = 0, classe = 0;
+            int toPlace = getPlaceRealTime(subjectIndex);
+ 
+            for(int i = 1; i < tweets.size(); i++) {
+                Review review = new Review();
+                review.setText(tweets.get(i));
+                classe += bag.analyzeReview(review);
+                
+                numberOfTweets++;
+            }
+            
+            classe /= numberOfTweets;
+            rates.get(subjectIndex)[toPlace] = classe;
+            numberAnalyzed.get(subjectIndex)[toPlace] = numberOfTweets;
+            
+            double[] day = new double[rates.get(0).length];
+            for(int i = 0; i < period.size(); i++)
+                day[i] = i;
+            g.removeData(namesOfSubjects.get(subjectIndex));
+            g.addData(namesOfSubjects.get(subjectIndex), day, rates.get(subjectIndex), numberAnalyzed.get(subjectIndex));
+            
+        } catch(Exception e) {
+            System.out.println("Real time analyze failed");
+            System.out.println(e.getMessage());
+        }
     }
     
     public void launchAnalyzeFiles() {
@@ -88,7 +136,7 @@ public class tweetAnalyze {
             bag.setFileName(bogFileName);
             bag = new BagOfWords(bag.deserialize());
             
-            setUp();
+            setUp(period.size());
             
             for(int i = 0; i < getNumberOfSubjects(); i++) {
                 for(int j = 0; j < period.size(); j++) {
@@ -132,12 +180,12 @@ public class tweetAnalyze {
     }
     
     
-    public void graphTest() {
-        Graph g = new Graph();
-        double[] day = new double[period.size()];
+    public void launchGraph() {
+        
         System.out.println("Graph test : ");
         System.out.println("Number of guys : " + getNumberOfSubjects());
         
+        double[] day = new double[rates.get(0).length];
         for(int i = 0; i < period.size(); i++)
             day[i] = i;
         
@@ -152,4 +200,32 @@ public class tweetAnalyze {
         g.display();
     }
     
+    public void refreshGraph() {
+        g.display();
+    }
+    
+    public int getPlaceRealTime(int subjectIndex) {
+        if(rates.get(subjectIndex).length == threshold) {
+            for(int j = 0; j < threshold-1; j++) {
+                rates.get(subjectIndex)[j] = rates.get(subjectIndex)[j+1];
+                numberAnalyzed.get(subjectIndex)[j] = numberAnalyzed.get(subjectIndex)[j+1];
+            }
+            
+            return threshold-1;
+        } else {
+            int size = rates.get(subjectIndex).length;
+            
+            double[] newR = new double[size+1];
+            double[] newN = new double[size+1];
+            
+            for(int i = 0; i < size; i++) {
+                newR[i] = rates.get(subjectIndex)[i];
+                newN[i] = numberAnalyzed.get(subjectIndex)[i];
+            }
+            
+            return size+1;
+            
+        }
+        
+    }
 }
